09e9e8c1f814678dabe2c0f86a14b40dc4c207ce
Gbenga <divadaforbusiness@gmail.com>
2024-08-27 14:31:40 +0100

    palindrome

---
+++ index.js
@@ -0,0 +1,39 @@
+const textInput = document.getElementById("text-input")
+const checkBtn = document.querySelector("#check-btn")
+
+
+checkBtn.addEventListener("click", ()=> {
+    if(!(/[a-zA-Z]/.test(textInput.value))){ 
+        ///[a-zA-Z]/ is a regular expression
+        alert("Please input a value")
+    } else {
+        const initTextValue = textInput.value
+        let textValue = textInput.value
+        let textArray = textValue.split('')
+
+         //returns values not equal to ' ' and their likes
+        textArray = textArray.filter(e => e !== ' ')
+        textArray = textArray.filter(e => e !== '_')
+        textArray = textArray.filter(e => e !== ':')
+        textArray = textArray.filter(e => e !== ';')
+        textArray = textArray.filter(e => e !== '(')
+        textArray = textArray.filter(e => e !== ')')
+        textArray = textArray.filter(e => e !== '-')
+        textArray = textArray.filter(e => e !== ',')
+        textArray = textArray.filter(e => e !== '.')
+        textArray = textArray.filter(e => e !== '/')
+        textArray = textArray.filter(e => e !== '/')
+
+        textValue = textArray.join('')
+        textValueReverse = textValue.split('').reverse().join('')
+        if (textValue === textValueReverse){
+            document.getElementById("result").innerHTML = `${initTextValue} is a Palindrome`
+        } else if (textValue !== textValueReverse){
+            document.getElementById("result").innerHTML = `${initTextValue} is not a Palindrome`
+        }
+        console.log(textValue)
+    }
+    
+    
+    /* This project has made me put to use the split(), reverse(), join(), a REGEX, filter() */
+})
